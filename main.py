ad81b06f038686167cd11729ba936faa8126834d
Jsvensson12 <jespersvensson1@live.com>
2021-12-01 12:21:13 +0100

    fixed

--- main.py
+++ main.py
@@ -5,26 +5,51 @@ from database import run, get
 run('''
   CREATE TABLE IF NOT EXISTS artist (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description STRING NOT NULL,
     artist_name STRING NOT NULL
+    
  )
 ''')
 
 # Album table
 run('''
   CREATE TABLE IF NOT EXISTS album (
-    artistid INTEGER PRIMARY KEY AUTOINCREMENT,
+    artist_id INTEGER,
     album_name STRING NOT NULL,
+    description STRING NOT NULL,
     year_release STRING,
-    FOREIGN KEY(artistid) REFERENCES arist(id)
+    FOREIGN KEY(artist_id) REFERENCES arist(id)
  )
 ''')
 
 # Songs table
 run('''
   CREATE TABLE IF NOT EXISTS songs (
-    albumid INTEGER PRIMARY KEY AUTOINCREMENT,
+    album_id INTEGER,
     song_name STRING NOT NULL,
     song_duration INTEGER NOT NULL,
-    FOREIGN KEY(albumid) REFERENCES album(id)
+    FOREIGN KEY(album_id) REFERENCES album(id)
  )
 ''')
+
+
+artists = [
+  {
+    'artist_name': 'Ed Sheeran',
+    'description': 'Pop',
+    
+  },
+  {
+    'artist_name': 'Elvis Presley',
+    'description': 'king of rock',
+  
+  },
+  {
+    'artist_name': 'Michael Jackson',
+    'description': 'king of pop',
+    
+  },
+]
+
+
+
