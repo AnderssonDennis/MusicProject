5ef751e2fe74c0402756133f94b35cf32ea914fe
Dennis Andersson <dennis_a12@hotmail.com>
2021-12-02 11:34:11 +0100

    Add function fixed

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ run('''
     artist_id INTEGER NOT NULL,
     album_name STRING NOT NULL,
     description STRING NOT NULL,
-    year_released STRING,
+    year_released INTEGER,
     FOREIGN KEY(artist_id) REFERENCES arist(id)
  )
 ''')
@@ -64,22 +64,22 @@ albums = [
   {
     'album_name': 'X',
     'description': 'Ed Sheerans album',
-    'year_released': '2014',
-    'artist_id': '1'
+    'year_released': 2014,
+    'artist_id': 1
     
   },
   {
     'album_name': 'Elvis christmas album',
     'description': 'Elvis most sold album',
-    'year_released': '1957',
-    'artist_id': '2'
+    'year_released': 1957,
+    'artist_id': 2
   
   },
   {
     'album_name': 'Bad',
     'description': 'Michaels badest album',
-    'year_released': '1987',
-    'artist_id': '3'
+    'year_released': 1987,
+    'artist_id': 3
   },
 ]
 
@@ -92,51 +92,51 @@ for mock_album in albums:
 songs_ed = [
   {
     'song_name': 'One',
-    'song_duration': '4:13',
+    'song_duration': 4.13,
     'artist_id': 1
   },
   {
     'song_name': 'Im a Mess',
-    'song_duration': '4:06',
+    'song_duration': 4.06,
     'artist_id': 1
   },
   {
     'song_name': 'Sing',
-    'song_duration': '3:55',
+    'song_duration': 3.55,
     'artist_id': 1
   },
   {
     'song_name': 'Dont',
-    'song_duration': '3:39',
+    'song_duration': 3.39,
     'artist_id': 1
   },
   {
     'song_name': 'Nina',
-    'song_duration': '3:43',
+    'song_duration': 3.43,
     'artist_id': 1
   },
   {
     'song_name': 'Photograph',
-    'song_duration': '4:17',
+    'song_duration': 4.17,
     'artist_id': 1
   },  {
     'song_name': 'Bloodstream',
-    'song_duration': '4:59',
+    'song_duration': 4.59,
     'artist_id': 1
   },
   {
     'song_name': 'Tenerife Sea',
-    'song_duration': '4:00',
+    'song_duration': 4.00,
     'artist_id': 1
   },
   {
     'song_name': 'Runaway',
-    'song_duration': '3:26',
+    'song_duration': 3.26,
     'artist_id': 1
   },   
   {
     'song_name': 'The Man',
-    'song_duration': '4:09',
+    'song_duration': 4.09,
     'artist_id': 1
   },
 ]
@@ -149,51 +149,51 @@ for mock_ed in songs_ed:
 songs_elvis = [
   {
     'song_name': 'Santa Claus Is Back In Town',
-    'song_duration': '2:26',
-    'artist_id': '2'
+    'song_duration': 2.26,
+    'artist_id': 2
   },
   {
     'song_name': 'White Christmas',
-    'song_duration': '2:25',
-    'artist_id': '2'
+    'song_duration': 2.25,
+    'artist_id': 2
   },
   {
     'song_name': 'Here Comes Santa Claus',
-    'song_duration': '1:56',
-    'artist_id': '2'
+    'song_duration': 1.56,
+    'artist_id': 2
   },
   {
     'song_name': 'Ill Be Home for Christmas',
-    'song_duration': '1:55',
-    'artist_id': '2'
+    'song_duration': 1.55,
+    'artist_id': 2
   },
   {
     'song_name': 'Blue Christmas',
-    'song_duration': '2:09',
-    'artist_id': '2'
+    'song_duration': 2.09,
+    'artist_id': 2
   },
   {
     'song_name': 'Santa Bring My Baby Back',
-    'song_duration': '1:54',
-    'artist_id': '2'
+    'song_duration': 1.54,
+    'artist_id': 2
   },  {
     'song_name': 'O Little Town of Bethlehem',
-    'song_duration': '2:37',
-    'artist_id': '2'
+    'song_duration': 2.37,
+    'artist_id': 2
   },
   {
     'song_name': 'Silent Night',
-    'song_duration': '2:25',
-    'artist_id': '2'
+    'song_duration': 2.25,
+    'artist_id': 2
   },
   {
     'song_name': 'Peace in the Valley',
-    'song_duration': '3:22',
-    'artist_id': '2'
+    'song_duration': 3.22,
+    'artist_id': 2
   },  {
     'song_name': 'I Believe',
-    'song_duration': '2:05',
-    'artist_id': '2'
+    'song_duration': 2.05,
+    'artist_id': 2
   },
 ]
 # Elvis albumlista
@@ -207,53 +207,84 @@ for mock_elvis in songs_elvis:
 songs_mj = [
   {
     'song_name': 'Bad',
-    'song_duration': '4:07',
-    'artist_id': '3'
+    'song_duration': 4.07,
+    'artist_id': 3
   },
   {
     'song_name': 'The Way You Make Me Feel',
-    'song_duration': '4:58',
-    'artist_id': '3'
+    'song_duration': 4.58,
+    'artist_id': 3
   },
   {
     'song_name': 'Speed Demon',
-    'song_duration': '4:01',
-    'artist_id': '3'
+    'song_duration': 4.01,
+    'artist_id': 3
   },
   {
     'song_name': 'Liberian Girl',
-    'song_duration': '3:53',
-    'artist_id': '3'
+    'song_duration': 3.53,
+    'artist_id': 3
   },
   {
     'song_name': 'Just Good Friends',
-    'song_duration': '4:05',
-    'artist_id': '3'
+    'song_duration': 4.05,
+    'artist_id': 3
   },
   {
     'song_name': 'Another Part of Me',
-    'song_duration': '3:53',
-    'artist_id': '3',
+    'song_duration': 3.53,
+    'artist_id': 3
   },  {
     'song_name': 'Man in the Mirror',
-    'song_duration': '5:18',
-    'artist_id': '3'
+    'song_duration': 5.18,
+    'artist_id': 3
   },
   {
     'song_name': 'I Just Can’t Stop Loving You',
-    'song_duration': '4:13',
-    'artist_id': '3'
+    'song_duration': 4.13,
+    'artist_id': 3
   },
   {
     'song_name': 'Dirty Diana',
-    'song_duration': '4:41',
-    'artist_id': '3'
+    'song_duration': 4.41,
+    'artist_id': 3
   },  {
     'song_name': 'Smooth Criminal',
-    'song_duration': '4:16',
-    'artist_id': '3'
+    'song_duration': 4.16,
+    'artist_id': 3
   },
 ]
 # Michael Jackson
 for mock_mj in songs_mj:
   run('INSERT INTO song VALUES(NULL, :artist_id, :song_name, :song_duration)', mock_mj)
+
+# select all albums with songs
+db_album = get('''
+  SELECT * FROM album
+  LEFT JOIN song
+  ON album.id = song.album_id
+''')
+
+# heroes is a dict where row id is the key for each hero.
+# this enables us to easily check if there's already a hero
+# with that id saved.
+album_check = {}
+for row in db_album:
+  print(row['id'], row['album_name'], row['song_name'])
+ # create album if not exists
+id = row['id']
+
+if album_check.get(id) == None:
+    album_check[id] = {
+      'album_name': row['Divide'],
+      'description': row['Eds most sold album'],
+      'year_released': row['2017'],
+       'artist_id': row['1'],
+      'songs': []
+    }
+# add song to album
+if row['song_name'] != None:
+    album_check[id]['songs'].append({
+      'song_name': row['Galway Girl'],
+      'song_duration' : row[2.50]
+    })
